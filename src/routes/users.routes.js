eaabd6fb3405743353436d663b97454869f309bd
nervinjf <109636023+nervinjf@users.noreply.github.com>
2025-09-04 12:01:59 -0400

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { userRegister, getAllUsers, verifyOTP,updateUserRoleAndStatus } = require("../controllers");
-const { authVerification, requireAdmin } = require("../middlewares/auth.middleware");
+const { authVerification } = require("../middlewares/auth.middleware");
 
 
 const router = Router();
@@ -9,7 +9,7 @@ router.get("/nebconnection/nestle/users",  authVerification, getAllUsers);
 router.post('/nebconnection/nestle/users', userRegister);
 router.post('/verifyOTP', verifyOTP);
 // PATCH: actualizar rol y estado activo de un usuario
-router.patch("/nebconnection/nestle/users/:id", authVerification, requireAdmin, updateUserRoleAndStatus);
+router.patch("/nebconnection/nestle/users/:id", authVerification, updateUserRoleAndStatus);
 
 
 module.exports = router;
