2dab3f67aa33b3bb81c76d6f0171353edc36ac7b
nervinjf <109636023+nervinjf@users.noreply.github.com>
2025-09-05 11:12:31 -0400

    Create sucursales.routes.js

---
+++ src/routes/sucursales.routes.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const upload = require("../middlewares/upload.single"); // usa tu propio middleware (multer)
+const {
+  CreateBulkBranches,
+  EditBulkBranches,
+  ListBranches,
+  CreateBranch,
+  UpdateBranch,
+  SearchBranch,
+} = require("../controllers/sucursales.controller");
+
+const router = Router();
+
+// Base: /nebconnection/nestle
+router.get("/nebconnection/nestle/sucursales", ListBranches);
+router.get("/nebconnection/nestle/sucursales/select", SearchBranch);
+router.post("/nebconnection/nestle/sucursales/bulk", upload.single("file"), CreateBulkBranches);
+router.put("/nebconnection/nestle/sucursales/bulk", upload.single("file"), EditBulkBranches);
+router.post("/nebconnection/nestle/sucursales", CreateBranch);
+router.put("/nebconnection/nestle/sucursales/:id", UpdateBranch);
+
+module.exports = router;
